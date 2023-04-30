027af48aba2b9c6a3e58453d2cfd42db3df9c513
Su Hlaing <suhlaing200341@gmail.com>
2023-04-30 21:39:18 +0630

    feat: add locations

--- frontend/src/components/Menus.tsx
+++ frontend/src/components/Menus.tsx
@@ -7,7 +7,11 @@ import { config } from "../config/config";
 import { Link } from "react-router-dom";
 
 const Menus = () => {
-  const { fetchData, menus } = UseAppContext();
+  const query = new URLSearchParams(window.location.search);
+  const locationId = query.get("locationId") as unknown as string;
+  console.log("locationId is...", locationId);
+
+  const { fetchData, menus, menuLocations } = UseAppContext();
   // console.log("Menus: ", menus);
 
   const [menu, setMenu] = useState<Menu | null>(null);
@@ -33,66 +37,82 @@ const Menus = () => {
     fetchData();
   };
 
+  const ValidMenuLocation = menuLocations
+    .filter((item) => String(item.location_id) === locationId)
+    .map((item) => item.menu_id);
+  console.log("ValidMenuLocation", ValidMenuLocation);
+
+  const filteredMenu = menus.filter((menu) =>
+    ValidMenuLocation.includes(menu.id as number)
+  );
+  console.log("menuLocation", filteredMenu);
+
   return (
     <Layout>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          margin: "0 auto",
-        }}
-      >
-        <Box
-          sx={{
-            mt: "2rem",
+      {locationId < "3" ? (
+        <div
+          style={{
             display: "flex",
-            columnGap: "1.5rem",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            margin: "0 auto",
           }}
         >
-          <TextField
-            label="Name"
-            onChange={(e) =>
-              setMenu({
-                name: e.target.value,
-                price: menu?.price ? menu.price : 0,
-              })
-            }
-            variant="outlined"
-          />
-          <TextField
-            label="Price"
-            variant="outlined"
-            onChange={(e) =>
-              setMenu({
-                name: menu?.name ? menu.name : "",
-                price: parseInt(e.target.value, 10),
-              })
-            }
-            type="number"
-          />
-        </Box>
-        <Box sx={{ mt: "2rem", mb: "1.8rem" }}>
-          <Button variant="contained" onClick={createMenu}>
-            Create
-          </Button>
-        </Box>
+          <Box
+            sx={{
+              mt: "2rem",
+              display: "flex",
+              columnGap: "1.5rem",
+            }}
+          >
+            <TextField
+              label="Name"
+              onChange={(e) =>
+                setMenu({
+                  name: e.target.value,
+                  price: menu?.price ? menu.price : 0,
+                })
+              }
+              variant="outlined"
+            />
+            <TextField
+              label="Price"
+              variant="outlined"
+              onChange={(e) =>
+                setMenu({
+                  name: menu?.name ? menu.name : "",
+                  price: parseInt(e.target.value, 10),
+                })
+              }
+              type="number"
+            />
+          </Box>
+          <Box sx={{ mt: "2rem", mb: "1.8rem" }}>
+            <Button variant="contained" onClick={createMenu}>
+              Create
+            </Button>
+          </Box>
 
-        <Stack direction="column" spacing={3}>
-          {menus.map((item) => {
-            return (
-              <Link key={item.id} to={`/menus/${item.id}`}>
-                <Chip
-                  label={item.name}
-                  sx={{ cursor: "pointer" }}
-                  onDelete={() => handleDelete(item.id)}
-                />
-              </Link>
-            );
-          })}
-        </Stack>
-      </div>
+          <Stack direction="column" spacing={3}>
+            {filteredMenu.map((item) => {
+              return (
+                <Link key={item.id} to={`/menus/${item.id}`}>
+                  <Chip
+                    label={item.name}
+                    sx={{ cursor: "pointer" }}
+                    onDelete={() => handleDelete(item.id)}
+                  />
+                </Link>
+              );
+            })}
+          </Stack>
+        </div>
+      ) : (
+        <Box>
+          <h1>Missing locationId</h1>
+        </Box>
+      )}
     </Layout>
   );
 };
--- frontend/src/components/Setting.tsx
+++ frontend/src/components/Setting.tsx
@@ -1,9 +1,76 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
 import Layout from "./Layout";
+import { UseAppContext } from "../contexts/AppContext";
+import { Locations } from "../typings/types";
+import { useEffect, useState } from "react";
 
 const Setting = () => {
+  const { locations } = UseAppContext();
+  console.log("Locations...", locations);
+
+  const [selectedLocation, setSelectedLocation] = useState<
+    Locations | undefined
+  >();
+  console.log("selectedLocation.....", selectedLocation);
+
+  useEffect(() => {
+    if (locations.length) {
+      const selectedLocationId = localStorage.getItem("selectedLocation");
+      if (!selectedLocationId) {
+        localStorage.setItem("selectedLocation", String(locations[0].id));
+        setSelectedLocation(locations[0]);
+      } else {
+        const selectedlocations = locations.find(
+          (location) => String(location.id) === selectedLocationId
+        );
+        // console.log("selectedlocations ", selectedlocations);
+        setSelectedLocation(selectedlocations);
+      }
+    }
+  }, [locations]);
+
+  const handleOnChange = (e: SelectChangeEvent<number>) => {
+    localStorage.setItem("selectedLocation", String(e.target.value));
+    const filteredLocation = locations.find(
+      (item) => item.id === e.target.value
+    );
+    // console.log("filteredLocation...", filteredLocation);
+    setSelectedLocation(filteredLocation);
+  };
+
   return (
     <Layout>
-      <h1>Setting Page</h1>
+      <div
+        style={{
+          width: "400px",
+          margin: "2rem auto",
+        }}
+      >
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Location</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedLocation ? selectedLocation.id : ""}
+            label="Location"
+            onChange={handleOnChange}
+          >
+            {locations.map((item) => {
+              return (
+                <MenuItem key={item.id} value={item.id}>
+                  {item.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </div>
     </Layout>
   );
 };
